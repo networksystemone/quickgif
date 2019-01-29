d279cd9db95efdee0389225ab213b96f82154fd5
kalebso <kalebso@protonmail.com>
2019-01-28 23:39:59 -0800

    cleaning up unused imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
-import { Button, Container, Row } from 'reactstrap';
-import FaYoutubePlay from 'react-icons/lib/fa/youtube-play';
-import FaFileMovieO from 'react-icons/lib/fa/file-movie-o';
+import { Container } from 'reactstrap';
 
 import NavBar from './components/NavBar';
 import LocalVideoContainer from './containers/LocalVideoContainer';
-import logo from './toast.svg';
 import './App.css';
 
 class App extends Component {
