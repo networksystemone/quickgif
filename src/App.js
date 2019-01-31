c35c40dfd100dcfd1f0b5c8788a19716e9c6e0ef
kalebso <kalebso@protonmail.com>
2019-01-31 00:22:47 -0800

    adding tachyons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Container } from 'reactstrap';
 import NavBar from './components/NavBar';
 import LocalVideoContainer from './containers/LocalVideoContainer';
+import './tachyons/css/tachyons.min.css';
 import './App.css';
 
 class App extends Component {
