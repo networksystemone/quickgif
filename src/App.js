1787f184a63ae299a53f940c2253965f96e52a5c
kalebso <kalebso@protonmail.com>
2019-02-02 16:47:30 -0800

    pull out logic into CreateGifContainer

--- src/App.js
+++ src/App.js
@@ -1,68 +1,13 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
-import { observer, inject } from 'mobx-react';
+import CreateGifContainer from './containers/CreateGifContainer';
 
-import { create } from './GifMaker';
-import FileDrop from './components/FileDrop';
-import VideoPreview from './components/VideoPreview';
-
-@inject('GifStore')
-@observer
 class App extends Component {
-  handleFile = file => {
-    this.props.GifStore.setFile(file);
-  };
-
-  setDuration = duration => {
-    this.props.GifStore.setDuration(duration);
-  };
-
-  setStartTime = startTime => {
-    this.props.GifStore.setStartTime(startTime);
-  };
-
-  createGif = () => {
-    create();
-  };
-
-  displayGif = () => {
+  render() {
     return (
       <div>
-        <img src={this.props.GifStore.gif.image} alt='gifImg' />
+        <CreateGifContainer />
       </div>
     );
-  };
-
-  render() {
-    if (this.props.GifStore.file === null) {
-      return (
-        <div>
-          <FileDrop file={this.handleFile} />
-        </div>
-      );
-    } else if (
-      this.props.GifStore.file !== null &&
-      this.props.GifStore.gif === null
-    ) {
-      return (
-        <div>
-          <div>
-            <VideoPreview
-              source={this.props.GifStore.file}
-              startTime={this.setStartTime}
-              duration={this.setDuration}
-            />
-          </div>
-          <div>
-            <Button onClick={this.createGif} color='success' size='md'>
-              Create Gif
-            </Button>
-          </div>
-        </div>
-      );
-    } else {
-      return <div>{this.displayGif()}</div>;
-    }
   }
 }
 
