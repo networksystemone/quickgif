39221edba13e883ff14a345a95eb7f70a3c0078f
kalebso <kalebso@protonmail.com>
2019-02-02 16:08:11 -0800

    removing tachyons

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'mobx-react';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'semantic-ui-css/semantic.min.css';
 import GifStore from './stores/GifStore';
 
 const Root = (
