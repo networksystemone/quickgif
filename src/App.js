3b8ece564d1b8c47ee0f2c2d9f523f8e9507e9ee
kalebso <kalebso@protonmail.com>
2019-02-03 19:44:22 -0800

    adding top menu nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
+import TopMenu from './components/TopMenu';
 import CreateGifContainer from './containers/CreateGifContainer';
 
 import './App.css';
 
 class App extends Component {
   render() {
-    return <CreateGifContainer />;
+    return (
+      <div>
+        <TopMenu />
+        <CreateGifContainer />
+      </div>
+    );
   }
 }
 
