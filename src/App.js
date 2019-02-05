430fe187732d0b50cef38805fd526d10d8e4bb4d
kalebso <kalebso@protonmail.com>
2019-02-04 22:37:05 -0800

    removing top menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-import TopMenu from './components/TopMenu';
 import CreateGifContainer from './containers/CreateGifContainer';
 
 import './App.css';
@@ -9,7 +8,6 @@ class App extends Component {
   render() {
     return (
       <div>
-        <TopMenu />
         <CreateGifContainer />
       </div>
     );
