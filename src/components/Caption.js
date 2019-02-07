b299fa927ffc3f4eb940d920c8ceab2a52d6290f
kalebso <kalebso@protonmail.com>
2019-02-06 23:16:57 -0800

    styling caption page

--- src/components/Caption.js
+++ src/components/Caption.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header, Segment, Form, Button, Input } from 'semantic-ui-react';
+import { Icon, Segment, Form, Button, Input } from 'semantic-ui-react';
 import { observer, inject } from 'mobx-react';
 import styled from 'styled-components';
 import { createGif } from '../GifMaker';
@@ -41,9 +41,11 @@ class Caption extends React.Component {
       );
     } else {
       return (
-        <div>
+        <div className='mb2'>
           <Segment placeholder>
-            <Header icon>Gif preview here </Header>
+            <h2 className='f4 f3-l fw2  ma1'>
+              Click "Preview" to see your gif
+            </h2>
           </Segment>
         </div>
       );
@@ -53,85 +55,103 @@ class Caption extends React.Component {
   render() {
     const { caption, fontSize, fontColor, baseline } = this.props.GifStore;
     return (
-      <div>
-        <SegementStyled>
-          <Header>3) Add a caption</Header>
-          <Form onSubmit={this.handleSubmit}>
-            <Form.Field>
-              <label>Caption</label>
-              <Input value={caption} onChange={this.handleCaptionChange} />
-            </Form.Field>
+      <Container>
+        <Wrapper className='gray'>
+          <h2 className='f4 f3-l fw2  ma1'>Let's add a caption!</h2>
+          <h2 className='f5 f4-l fw2  ma1'>
+            Click "preview" when you're ready to see your changes
+          </h2>
+          <Segment>
+            {this.displayGif()}
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Field>
+                <label>Caption</label>
+                <Input value={caption} onChange={this.handleCaptionChange} />
+              </Form.Field>
 
-            <Form.Field>
-              <label>Text Color</label>
-              <Button.Group>
-                <Button
-                  active={fontColor === '#000000'}
-                  value='#000000'
-                  onClick={this.handleColorChange}
-                >
-                  Black
-                </Button>
-                <Button
-                  active={fontColor === '#ffffff'}
-                  value='#ffffff'
-                  onClick={this.handleColorChange}
-                >
-                  White
-                </Button>
-              </Button.Group>
-            </Form.Field>
+              <Form.Field className='gray'>
+                <label>Text Color</label>
+                <Button.Group>
+                  <Button
+                    active={fontColor === '#000000'}
+                    value='#000000'
+                    onClick={this.handleColorChange}
+                  >
+                    Black
+                  </Button>
+                  <Button
+                    active={fontColor === '#ffffff'}
+                    value='#ffffff'
+                    onClick={this.handleColorChange}
+                  >
+                    White
+                  </Button>
+                </Button.Group>
+              </Form.Field>
 
-            <Form.Field>
-              <label>Baseline</label>
-              <Button.Group>
-                <Button
-                  active={baseline === 'bottom'}
-                  value='bottom'
-                  onClick={this.handleBaseline}
-                >
-                  Bottom
-                </Button>
-                <Button
-                  active={baseline === 'center'}
-                  value='center'
-                  onClick={this.handleBaseline}
-                >
-                  Center
-                </Button>
-                <Button
-                  active={baseline === 'top'}
-                  value='top'
-                  onClick={this.handleBaseline}
-                >
-                  Top
-                </Button>
-              </Button.Group>
-            </Form.Field>
+              <Form.Field>
+                <label>Location</label>
+                <Button.Group>
+                  <Button
+                    active={baseline === 'bottom'}
+                    value='bottom'
+                    onClick={this.handleBaseline}
+                  >
+                    Bottom
+                  </Button>
+                  <Button
+                    active={baseline === 'center'}
+                    value='center'
+                    onClick={this.handleBaseline}
+                  >
+                    Center
+                  </Button>
+                  <Button
+                    active={baseline === 'top'}
+                    value='top'
+                    onClick={this.handleBaseline}
+                  >
+                    Top
+                  </Button>
+                </Button.Group>
+              </Form.Field>
 
-            <Form.Field>
-              <label>FontSize</label>
-              <Input
-                type='number'
-                label={{ basic: true, content: 'px' }}
-                labelPosition='right'
-                value={fontSize}
-                onChange={this.handleFontSizeChange}
-              />
-            </Form.Field>
-            <Button type='submit'>Preview</Button>
-          </Form>
-        </SegementStyled>
-        {this.displayGif()}
-      </div>
+              <Form.Field>
+                <label>FontSize</label>
+                <Input
+                  type='number'
+                  label={{ basic: true, content: 'px' }}
+                  labelPosition='right'
+                  value={fontSize}
+                  onChange={this.handleFontSizeChange}
+                />
+              </Form.Field>
+              <Button color={'grey'} type='submit'>
+                Preview
+              </Button>
+            </Form>
+          </Segment>
+          <Button color={'teal'}>
+            <Icon name='download' color='white' /> Download Gif
+          </Button>
+        </Wrapper>
+      </Container>
     );
   }
 }
 
-const SegementStyled = styled(Segment)`
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  max-width: 600px;
+  margin: 1rem;
+`;
+
+const Container = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
-  flex-direction: column;
+  margin-top: 2rem;
 `;
+
 export default Caption;
